35a6b9bfb9316b458c214dd36ef4fd0ce16d65c9
Jovel960 <jovel960@icloud.com>
2022-11-15 17:58:54 +0200

    Complete task 8

--- yovel.java
+++ yovel.java
@@ -1,5 +1,5 @@
 public class yovel {
 public static void main(String[] args){
-    String name = "Yovel Hadad updating!";
+    String name = "Yovel Hadad updating again!";
 for (int i=0; i<10 ; i++) { System.out.println(name);}
 }}
