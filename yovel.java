076adeaaf135a29ddc138881aaa566f8ca6b080c
Jovel960 <jovel960@icloud.com>
2022-11-15 18:07:54 +0200

    Update file

--- yovel.java
+++ yovel.java
@@ -1,5 +1,5 @@
 public class yovel {
 public static void main(String[] args){
-    String name = "Yovel Hadad updating again 4!";
+    String name = "Yovel Hadad updating!";
 for (int i=0; i<10 ; i++) { System.out.println(name);}
 }}
